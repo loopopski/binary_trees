acd0a19f85faa82937178f438d2c69c3caa37a82
Same Titan <49342907+loopopski@users.noreply.github.com>
2022-09-20 21:40:29 +0100

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,43 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - finds the lowest common ancestor of two nodes
- * @first: the first node
- * @second: the second node
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
  *
- * Return:  lowest common ancestor node of the two given nodes
+ * Return: If no common ancestors return NULL, else return common ancestor.
  */
-binary_tree_t
-*binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-	binary_tree_t **path = NULL;
-	binary_tree_t *ancestor = NULL;
-	int x = 0, i;
+	binary_tree_t *mom, *pop;
 
-	if (first == NULL || second == NULL)
+	if (!first || !second)
 		return (NULL);
-	path = malloc(sizeof(*path));
-	while (first != NULL)
-	{
-		path[x++] = (binary_tree_t *)first;
-		first = first->parent;
-		if (first == NULL)
-			break;
-		path = realloc(path, sizeof(*path) * (x + 1));
-	}
-	while (second != NULL)
-	{
-		for (i = 0; i < x; i++)
-		{
-			if (second == path[i])
-			{
-				ancestor = path[i];
-				free(path);
-				return (ancestor);
-			}
-		}
-		second = second->parent;
-	}
-	free(path);
-	return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
 }
